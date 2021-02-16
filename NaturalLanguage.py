98b88047259938a435e0984760cf1c3e32384553
r-zh <tonyzhou@live.cn>
2021-02-15 23:06:31 -0500

    Add loop to restart the language analysis

--- NaturalLanguage.py
+++ NaturalLanguage.py
@@ -1,26 +1,34 @@
 import nltk
-from sqlparse.filters import output
+from nltk.sentiment.vader import SentimentIntensityAnalyzer
 
-sentences = "At eight o'clock on Thursday morning Arthur didn't feel very good."
 
-tokens = nltk.word_tokenize(sentences)
 
-#>>> tokens
-#['At', 'eight', "o'clock", 'on', 'Thursday', 'morning', 'Arthur', 'did', "n't", 'feel', 'very', 'good', '.']
+if __name__ == "__main__":
 
-tagged = nltk.pos_tag(tokens)
+    sentences = "At eight o'clock on Thursday morning Arthur didn't feel very good."
 
-#tagged[0:6]
-#[('At', 'IN'), ('eight', 'CD'), ("o'clock", 'JJ'), ('on', 'IN'),('Thursday', 'NNP'), ('morning', 'NN')]
+    tokens = nltk.word_tokenize(sentences)
 
-for i in range(len(sentences)):
-    print(sentences[i])
-    print(len(sentences[i]))
+    #>>> tokens
+    #['At', 'eight', "o'clock", 'on', 'Thursday', 'morning', 'Arthur', 'did', "n't", 'feel', 'very', 'good', '.']
 
-    ss = sid.polarity_scores(sentences[i])
+    tagged = nltk.pos_tag(tokens)
 
-    print(ss)
-    print(ss['neg'])
-    print(ss['neu'])
-    print(ss['pos'])
+    #tagged[0:6]
+    #[('At', 'IN'), ('eight', 'CD'), ("o'clock", 'JJ'), ('on', 'IN'),('Thursday', 'NNP'), ('morning', 'NN')]
+
+    sid = SentimentIntensityAnalyzer()
+
+    while sentences != "end":
+        ss = sid.polarity_scores(sentences)
+        print(ss)
+        sentences = input("please enter the next sentence: ")
+
+'''
+    for i in range(len(sentences)):
+        print(sentences[i])
+        print(len(sentences[i]))
+'''
+
+
 
